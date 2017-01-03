93d06d0160bf7bc7df197e6a528a1960b3e4f56f
peel3r <ismc2222@gmail.com>
2017-01-03 14:39:07 +0000

    completed roman numerals

--- lib/romannumerals.rb
+++ lib/romannumerals.rb
@@ -1,3 +1,27 @@
 class RomanNumerals
 
+  ROMAN_NUMERALS = {
+
+                    1000 => "M",
+                    900 => "CM",
+                    500 => "D",
+                    400 => "CD",
+                    100 => "C",
+                    90 => "XC",
+                    50 => "L",
+                    40 => "XL",
+                    10 => "X",
+                    5 => "V",
+                    1 =>  "I"
+
+  }
+  def convert num
+    r_numeral = ""
+
+    ROMAN_NUMERALS.each do |key, val|
+      (num / key).times {r_numeral << val; num -= key}
+    end
+
+    r_numeral
+  end
 end
--- spec/romannumerals_spec.rb
+++ spec/romannumerals_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe RomanNumerals do
+subject(:roman) {RomanNumerals.new}
+  it 'converts 1 to I' do
+    expect(roman.convert(1)).to eq "I"
+  end
 
+  it 'converts 5 to V' do
+    expect(roman.convert(5)).to eq "V"
+  end
+
+  it 'converts 10 to X' do
+    expect(roman.convert(10)).to eq "X"
+  end
+
+  it 'converts 35 to XXXV' do
+    expect(roman.convert(35)).to eq "XXXV"
+  end
+
+  it 'converts 997 to CMXCVII' do
+    expect(roman.convert(997)).to eq "CMXCVII"
+  end
 end
